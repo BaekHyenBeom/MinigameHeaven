b27ae1a379d50bd239bbf82ed9ba59b29b810220
강수지 <lemontea1321@naver.com>
2024-06-21 12:12:33 +0900

    Add SwimSwim
    Add SwimSwim + Scene, Controller.cs, MiniGameManager.cs, ObstaclePool.cs, Obstacle.cs, Obstacle.prefab, Player.prefab

---
+++ Assets/02_Scripts/Controller/Controllers/SwimSwimController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SwimSwimController : MonoBehaviour, IController
+{
+    public Rigidbody2D rigid;
+    public float curMovementInput;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigid.AddForce(Vector2.right * curMovementInput * 1, ForceMode2D.Impulse);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            if (Time.timeScale == 0.0f) { return; }
+            rigid.velocity = new Vector2 (rigid.velocity.x, 0f);
+            rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<float>();
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = 0f;
+        }
+    }
+}
---
+++ Assets/02_Scripts/SwimSwimObstacle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimSwimObstacle : MonoBehaviour
+{
+    public SwimSwimMiniGameManager swimSwimMiniGameManager;
+    float curSpeed = 0.1f;
+
+    public void Awake()
+    {
+        swimSwimMiniGameManager = GetComponent<SwimSwimMiniGameManager>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CurSpeedChange());
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * curSpeed;
+    }
+
+    IEnumerator CurSpeedChange()
+    {
+        curSpeed = Random.Range(0.05f, 0.15f);
+        yield return new WaitForSeconds(150.0f); // 5초마다 변속: 프레임이 30이라면 150, 60이라면 300
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("게임 오버!");
+            swimSwimMiniGameManager.GameOver();
+        }
+    }
+}
