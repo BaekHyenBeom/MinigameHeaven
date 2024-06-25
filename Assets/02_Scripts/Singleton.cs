374bed253c7c4a7f507316211e97a0c1f4396230
BaekHyenBeom <167046656+BaekHyenBeom@users.noreply.github.com>
2024-06-25 14:51:45 +0900

    Update : 싱글톤 매니저들 겹칩 제가
    메인씬에서 생성되었던 매니저들이 다시 메인 씬으로 돌아올 때 2개가 되는 현상을 제거했습니다.

--- Assets/02_Scripts/Singleton.cs
+++ Assets/02_Scripts/Singleton.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+    private static List<string> dontDestroyObjects = new List<string>();
 
     public static T Instance
     {
@@ -26,6 +28,15 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
     protected virtual void Awake()
     {
-        DontDestroyOnLoad(Instance);
+        if (dontDestroyObjects.Contains(gameObject.name))
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            dontDestroyObjects.Add(gameObject.name);
+            DontDestroyOnLoad(Instance);
+        }
     }
 }
